86104806c7850e728a7f67e4f26e09aefb5ee343
koki <myaddressisthis123@gmail.com>
2019-08-18 22:59:34 +0900

    feat: 1つのTreadPoolServiceだとメモリが圧迫されて他のスレッドがブロックされて進まないこと

--- src/main/java/OOMExecutor.java
+++ src/main/java/OOMExecutor.java
@@ -1,14 +1,19 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.*;
+import java.util.function.Consumer;
 
 
 public class OOMExecutor {
 
     public static void main(String[] args) {
-        new OOMExecutor().detectLimitByteSize();
+        OOMExecutor tester = new OOMExecutor();
+        tester.detectLimitByteSize(b -> {
+        });
+        tester.threadOOM();
     }
 
-    void detectLimitByteSize() {
+    void detectLimitByteSize(Consumer<Long> perLoopConsumer) {
         List<byte[]> buffers = new ArrayList<>();
         long byteSum = 0;
         try {
@@ -16,6 +21,8 @@ public class OOMExecutor {
                 int byteMemory = 1024 * 1024;
                 buffers.add(new byte[byteMemory]);
                 byteSum += byteMemory;
+
+                perLoopConsumer.accept(byteSum);
             }
         } catch (OutOfMemoryError e) {
             System.out.println("byteSum が totalMemory を超えるあたりで[OutOfMemoryError: Java heap space]になることがわかる");
@@ -24,4 +31,21 @@ public class OOMExecutor {
         }
     }
 
+    // 1つのTreadPoolServiceだとメモリが圧迫されて他のスレッドがブロックされて進まないこと
+    void threadOOM() {
+        ExecutorService service = Executors.newFixedThreadPool(2);
+
+        Runnable detectLimitByteSize = () -> {
+            detectLimitByteSize(b -> {
+                System.out.println("zzz..." + Thread.currentThread().getName());
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            });
+        };
+        service.execute(detectLimitByteSize);
+        service.execute(detectLimitByteSize);
+    }
 }
